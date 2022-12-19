ee7dd66241b040f09b20fd5ac24686fc1615238a
João Portas <80261184+JPortas@users.noreply.github.com>
2022-12-19 15:28:27 +0000

    Example data file

---
+++ example-values.lisp
@@ -0,0 +1,23 @@
+;;;; example-values.lisp
+;;;; Funções que retornam valores para testes no projeto.
+;;;; Autor: Lucas Freixieiro e João Portas
+
+(defpackage :example-values
+    (:use
+        #:COMMON-LISP
+    )
+    (:export
+        #:ex-board
+    )
+)
+
+(in-package :example-values)
+
+;;; (tabuleiro-teste)
+(defun ex-board ()
+  "Retorna um tabuleiro 3x3 (3 arcos na vertical por 3 arcos na horizontal)"
+	'(
+		((0 0 0) (0 0 1) (0 1 1) (0 0 1))
+		((0 0 0) (0 1 1) (1 0 1) (0 1 1))
+	)
+)
