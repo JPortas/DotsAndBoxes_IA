ee05cf936ec5954f6874de41a07ad9392efb4f8d
João Portas <80261184+JPortas@users.noreply.github.com>
2022-12-20 14:52:14 +0000

    Heuristics avaliation option added

--- node.lisp
+++ node.lisp
@@ -14,13 +14,34 @@
         #:sucessors-to-vertical
         #:get-successors
         #:heuristic-eval-by-remaining-to-close
+        #:heuristic-eval-by-remaining-arcs
+        #:eval-heuristic
+        #:count-zeros
+        #:sucessors-to-horizontal-with-heuristic
+        #:sucessors-to-vertical-with-heuristic
     )
 )
 
 (in-package :node)
 
-(defun heuristic-eval-by-remaining-to-close (objective-close-squares current-closed-squares)
-    (- objective-close-squares current-closed-squares)
+(defun heuristic-eval-by-remaining-to-close (board objective-close-squares)
+    (- objective-close-squares (get-number-of-closed-boxes board))
+)
+
+(defun heuristic-eval-by-remaining-arcs (board)
+    (funcall 'count-zeros board)
+)
+
+(defun eval-heuristic (fn-heuristic &rest rest)
+    (cond
+        (
+            (and (symbolp fn-heuristic) (functionp (symbol-function fn-heuristic)) (not (null rest)))
+            (apply fn-heuristic rest)
+        )
+        (T
+            NIL
+        )
+    )
 )
 
 (defun new-successor (board depth parent &optional heuristic)
@@ -98,7 +119,7 @@ Se for Null retorna NIL. Se a heuristica não existir retorna NIL."
     )
 )
 
-(defun get-successors (node &optional fn-heuristic)
+(defun get-successors (node &optional fn-heuristic &rest rest)
 "Obtem e retorna todos os sucessores do no recebidos."
 ;Heuristica em desenvolvimento
     (cond
@@ -107,7 +128,27 @@ Se for Null retorna NIL. Se a heuristica não existir retorna NIL."
             (append (funcall 'sucessors-to-horizontal node) (funcall 'sucessors-to-vertical node))
         )
         (T
-            (funcall fn-heuristic 5 3)
+            ;(apply 'eval-heuristic fn-heuristic rest)
+            (append
+                (apply 'sucessors-to-horizontal-with-heuristic
+                    node
+                    (list-length (get-horizontal-arcs (get-node-state node)))
+                    (list-length (get-vertical-arcs (get-node-state node)))
+                    1
+                    1
+                    fn-heuristic
+                    rest
+                )
+                (apply 'sucessors-to-vertical-with-heuristic
+                    node
+                    (list-length (get-horizontal-arcs (get-node-state node)))
+                    (list-length (get-vertical-arcs (get-node-state node)))
+                    1
+                    1
+                    fn-heuristic
+                    rest
+                )
+            )
         )
     ) 
 )
@@ -212,6 +253,114 @@ Se for igual ou maior retorna T. Se for menor retorna NIL
         )
         (T
             (sucessors-to-vertical node horizontal-length vertical-length 1 (+ current-vertical-length 1))
+        )
+    )
+)
+
+(defun sucessors-to-horizontal-with-heuristic (node &optional (horizontal-length (list-length (get-horizontal-arcs (get-node-state node)))) (vertical-length (list-length (get-vertical-arcs (get-node-state node)))) (current-horizontal-length 1) (current-vertical-length 1) fn-heuristic &rest rest)
+"Gera os sucessores da colocação horizontal de uma jogada em qualquer nó que ainda não esteja preenchido. A função retorna uma lista com os sucessores."    
+    (cond
+        (
+            (> current-horizontal-length horizontal-length)
+            NIL
+        )
+        (
+            (< current-vertical-length vertical-length)
+            ;(format t "~%~d ~d~%" current-horizontal-length current-vertical-length)
+            ;(sucessors-to-horizontal node horizontal-length vertical-length current-horizontal-length (+ current-vertical-length 1))
+            (cond
+                (
+                    (null
+                        (draw-horizontal-arc
+                            (get-node-state node)
+                            current-horizontal-length
+                            current-vertical-length
+                        )
+                    )
+                    (apply 'sucessors-to-horizontal-with-heuristic node horizontal-length vertical-length current-horizontal-length (+ current-vertical-length 1) fn-heuristic rest)
+                )
+                (T
+                    (cons
+                        (new-successor
+                            (draw-horizontal-arc
+                                (get-node-state node)
+                                current-horizontal-length
+                                current-vertical-length
+                            )
+                            (+ (get-node-depth node) 1)
+                            node
+                            (apply 'eval-heuristic fn-heuristic (append (list (draw-horizontal-arc (get-node-state node) current-horizontal-length current-vertical-length)) rest))
+                        )
+                        (apply 'sucessors-to-horizontal-with-heuristic node horizontal-length vertical-length current-horizontal-length (+ current-vertical-length 1) fn-heuristic rest)
+                    )
+                )
+            )
+        )
+        (T
+            (apply 'sucessors-to-horizontal-with-heuristic node horizontal-length vertical-length (+ current-horizontal-length 1) 1 fn-heuristic rest)
+        )
+    )
+)
+
+(defun sucessors-to-vertical-with-heuristic (node &optional (horizontal-length (list-length (get-horizontal-arcs (get-node-state node)))) (vertical-length (list-length (get-vertical-arcs (get-node-state node)))) (current-horizontal-length 1) (current-vertical-length 1) fn-heuristic &rest rest)
+"Gera os sucessores da colocação vertical de uma jogada em qualquer nó que ainda não esteja preenchido. A função retorna uma lista com os sucessores."   
+    (cond
+        (
+            (> current-vertical-length vertical-length)
+            NIL
+        )
+        (
+            (< current-horizontal-length horizontal-length)
+            (cond
+                (
+                    (null
+                        (draw-vertical-arc
+                            (get-node-state node)
+                            current-horizontal-length
+                            current-vertical-length
+                        )
+                    )
+                    (apply 'sucessors-to-vertical-with-heuristic node horizontal-length vertical-length (+ current-horizontal-length 1) current-vertical-length fn-heuristic rest)
+                )
+                (T
+                    (cons
+                        (new-successor
+                            (draw-vertical-arc
+                                (get-node-state node)
+                                current-horizontal-length
+                                current-vertical-length
+                            )
+                            (+ (get-node-depth node) 1)
+                            node
+                            (apply 'eval-heuristic fn-heuristic (append (list (draw-vertical-arc (get-node-state node) current-horizontal-length current-vertical-length)) rest))
+                        )
+                        (apply 'sucessors-to-vertical-with-heuristic node horizontal-length vertical-length (+ current-horizontal-length 1) current-vertical-length fn-heuristic rest)
+                    )
+                )
+            )
+        )
+        (T
+            (apply 'sucessors-to-vertical-with-heuristic node horizontal-length vertical-length 1 (+ current-vertical-length 1) fn-heuristic rest)
+        )
+    )
+)
+
+(defun count-zeros (lst)
+    (cond
+        (
+            (null lst)
+            0
+        )
+        (
+            (numberp (car lst))
+            (if (zerop (car lst))
+                (+ 1 (count-zeros (cdr lst)))
+                (count-zeros (cdr lst)))) 
+        (T
+            (+ 
+                (count-zeros (car lst)) 
+                (count-zeros (cdr lst))
+            )
         )
     )
 )
